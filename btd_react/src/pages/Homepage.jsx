f3d805f4a7ec48551b392088b157989a7da213bc
aaron-webdev <aaronclure.webdev@gmail.com>
2025-05-20 18:47:29 -0400

    Navebar comonent work

--- btd_react/src/pages/Homepage.jsx
+++ btd_react/src/pages/Homepage.jsx
@@ -1,6 +1,9 @@
-import Container from "react-bootstrap/esm/Container";
-import Navigation from "../componets/Navigation";
+import Container from 'react-bootstrap/esm/Container';
+import Navigation from '../componets/Navigation';
 import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
+
+import Cookies1 from './../assets/cookies_in_oven.jpg'
 
 
 
@@ -15,13 +18,27 @@ export default function Home() {
   return (
     <>
       <Navigation/>
+
       <Container>
         <Carousel>
-          <Carousel.Item>
-
-          </Carousel.Item>
+          <Card>
+            <Card.Img src={Cookies1}/>
+          </Card>
+          
         </Carousel>
       </Container>
     </>
   )
 }
+
+/*
+        <Carousel>
+          <Carousel.Item>
+            <ExampleCarouselImage src={cookies1} />
+            <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+*/
