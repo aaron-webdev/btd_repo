0b0a89acffebf46660d91c6084f4a2696d672a74
aaron-webdev <aaronclure.webdev@gmail.com>
2025-05-20 23:33:54 -0400

    completed first carousel item with text in correct spot.

--- btd_react/src/componets/Showcase.jsx
+++ btd_react/src/componets/Showcase.jsx
@@ -14,7 +14,7 @@ export default function Showcase()
                 <Carousel.Item>
                     <img className='carousel-image showcase_image'src={Cookies1}/>
                     <Carousel.Caption style={{position:'absolute', top:'20%',bottom:'auto',textShadow: "2px 2px 4px rgba(0, 0, 0, 1)"}}>
-                        <h3>'You can taste the quality, the love, and the care that goes into each of Bake That Dough's products!'</h3>
+                        <h3>"You can taste the quality, the love, and the care that goes into each of Bake That Dough's products!"</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
