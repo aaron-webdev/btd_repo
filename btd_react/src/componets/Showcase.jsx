68e60b7c60ec464342b4f084670937fdc417df58
aaron-webdev <aaronclure.webdev@gmail.com>
2025-05-21 00:40:59 -0400

    completed third slide.

--- btd_react/src/componets/Showcase.jsx
+++ btd_react/src/componets/Showcase.jsx
@@ -1,5 +1,5 @@
 import Carousel from 'react-bootstrap/Carousel';
-
+import BiscuitGlam from './../assets/biscuit_glam.png';
 import Cookies1 from './../assets/cookies_in_oven.jpg'
 import GiftBox from './../assets/gift_box.png'
 import "./Showcase.css"
@@ -12,6 +12,7 @@ export default function Showcase()
 
     return(
             <Carousel>
+
                 <Carousel.Item interval={8000}>
                     <img className='carousel-image showcase_image'id='slide1' src={Cookies1}/>
                     <Carousel.Caption style={{position:'absolute', top:'20%',bottom:'auto',textShadow: "2px 2px 4px rgba(0, 0, 0, 1)"}}>
@@ -22,7 +23,14 @@ export default function Showcase()
                 <Carousel.Item>
                     <img className='carousel-image showcase_image'id='slide2' src={GiftBox}/>
                     <Carousel.Caption style={{position:'absolute', top:'20%',bottom:'auto',textShadow: "2px 2px 4px rgba(0, 0, 0, 1)"}}>
-                        <h3>"Great gift for Mother's Day!  These cookies absolutly made my day."</h3>
+                        <h3>"Great gift for Mother's Day!  These cookies absolutely made my day."</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img className='carousel-image showcase_image'id='slide3' src={BiscuitGlam}/>
+                    <Carousel.Caption style={{position:'absolute', top:'20%',bottom:'auto',textShadow: "2px 2px 4px rgba(0, 0, 0, 1)"}}>
+                        <h3>"The take and bake is perfect for surprising my family with delicious goodies."</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
 
