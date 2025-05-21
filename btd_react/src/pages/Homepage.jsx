c09b18d7813048376cc3b5b4a856c080e6e7a431
aaron-webdev <aaronclure.webdev@gmail.com>
2025-05-21 02:15:47 -0400

    deleted failed attempt at the carousel.

--- btd_react/src/pages/Homepage.jsx
+++ btd_react/src/pages/Homepage.jsx
@@ -16,25 +16,3 @@ export default function Home() {
     </>
   )
 }
-
-/*
-
-
-      <Container>
-        <Carousel>
-          <Card>
-            <Card.Img src={Cookies1}/>
-          </Card>
-        </Carousel>
-      </Container>
-
-        <Carousel>
-          <Carousel.Item>
-            <ExampleCarouselImage src={cookies1} />
-            <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-*/
