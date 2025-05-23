8636401a01ef213ac62dbc3af1a453041336f775
aaron-webdev <aaronclure.webdev@gmail.com>
2025-05-22 22:05:51 -0400

    changed seasonal preorder cards into a grid.

--- btd_react/src/pages/SeasonalPreorder.jsx
+++ btd_react/src/pages/SeasonalPreorder.jsx
@@ -1,6 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import HotCross from './../assets/hot_cross_buns.gif';
 import CinnRolls from './../assets/cinnamon_rolls.gif';
 import CarrotCake from './../assets/carrot_cake.png';
@@ -15,37 +17,47 @@ export default function SeasonalPreorder()
     return(
         <div>
             <Container id='EasterPreorder'>
-                <Card stlye={{width:'10rem'}}>
-                    <Card.Img src={HotCross}/>
-                    <Card.Body>
-                        <Card.Title>Hot Cross Buns</Card.Title>
-                        <Card.Subtitle>$12.00</Card.Subtitle>
-                    </Card.Body>
-                </Card>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Img src={HotCross}/>
+                            <Card.Body>
+                                <Card.Title>Hot Cross Buns</Card.Title>
+                                <Card.Subtitle>$12.00</Card.Subtitle>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card >
+                            <Card.Img src={CinnRolls}/>
+                            <Card.Body>
+                                <Card.Title>Cinnamon Rolls</Card.Title>
+                                <Card.Subtitle>$16.00</Card.Subtitle>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
 
-                <Card stlye={{width:'15rem'}}>
-                    <Card.Img src={CinnRolls}/>
-                    <Card.Body>
-                        <Card.Title>Cinnamon Rolls</Card.Title>
-                        <Card.Subtitle>$16.00</Card.Subtitle>
-                    </Card.Body>
-                </Card>
-
-                <Card stlye={{width:'15rem'}}>
-                    <Card.Img src={CarrotCake}/>
-                    <Card.Body>
-                        <Card.Title>Carrot Cake</Card.Title>
-                        <Card.Subtitle>$16.00</Card.Subtitle>
-                    </Card.Body>
-                </Card>
-
-                <Card stlye={{width:'15rem'}}>
-                    <Card.Img src={PullApart}/>
-                    <Card.Body>
-                        <Card.Title>Pull Apart Dinner Rolls</Card.Title>
-                        <Card.Subtitle>$10.00</Card.Subtitle>
-                    </Card.Body>
-                </Card>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Img src={CarrotCake}/>
+                            <Card.Body>
+                                <Card.Title>Carrot Cake</Card.Title>
+                                <Card.Subtitle>$16.00</Card.Subtitle>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img src={PullApart}/>
+                            <Card.Body>
+                                <Card.Title>Pull Apart Dinner Rolls</Card.Title>
+                                <Card.Subtitle>$10.00</Card.Subtitle>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
             </Container>
         </div>
     )
