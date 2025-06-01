01864c0aa76426f76e971f287624a1a3aa0b29df
aaron-webdev <aaronclure.webdev@gmail.com>
2025-06-01 12:35:56 -0400

    added market info

--- btd_react/src/componets/MarketInfo.jsx
+++ btd_react/src/componets/MarketInfo.jsx
@@ -12,7 +12,81 @@ export default function MarketInfo()
         <>
             <Card>
                 <Card.Title>Market Square Farmer's Market</Card.Title>
+                <Card.Subtitle>Knxoville</Card.Subtitle>
+                <Card.Body>
+                    <Card.Text>
+                        9am-1pm<br/>
+                        Every Saturday
+                    </Card.Text>
+                    <Button href='#' varient='success'>Directions to the market</Button>
+                </Card.Body>
+            </Card>
+
+            <Card>
+                <Card.Title>Jackson Square Farmer's Market</Card.Title>
+                <Card.Subtitle>Oak Ridge</Card.Subtitle>
+                <Card.Body>
+                    <Card.Text>
+                        8am-12p<br/>
+                        Every Saturday
+                    </Card.Text>
+                    <Button href='#' varient='success'>Directions to the market</Button>
+                </Card.Body>
+            </Card>
+
+            <Card>
+                <Card.Title>Founder's Square Farmer's Market</Card.Title>
+                <Card.Subtitle>Maryville</Card.Subtitle>
+                <Card.Body>
+                    <Card.Text>
+                        9am-1pm<br/>
+                        Every Saturday<br/><br/>
+                        10a-1p<br/>
+                        Every Wednesday
+                    </Card.Text>
+                    <Button href='#' varient='success'>Directions to the market</Button>
+                </Card.Body>
+            </Card>
+
+            <Card>
+                <Card.Title>Chattanooga Farmer's Market</Card.Title>
+                <Card.Subtitle>First Horizon Pavillion</Card.Subtitle>
+                <Card.Body>
+                    <Card.Text>
+                        11am-4pm<br/>
+                        First and third Sundays
+                    </Card.Text>
+                    <Button href='#' varient='success'>Directions to the market</Button>
+                </Card.Body>
+            </Card>
+
+            <Card>
+                <Card.Title></Card.Title>
+                <Card.Subtitle>Tellico Village Farmer's Market</Card.Subtitle>
+                <Card.Body>
+                    <Card.Text>
+                        9am-12pm<br/>
+                        Wednesdays
+                    </Card.Text>
+                    <Button href='#' varient='success'>Directions to the market</Button>
+                </Card.Body>
+            </Card>
+
+            <Card>
+                <Card.Title></Card.Title>
+                <Card.Subtitle>Maryville</Card.Subtitle>
+                <Card.Body>
+                    <Card.Text>
+                        3p-6p<br/>
+                        Tuesdays
+                    </Card.Text>
+                    <Button href='#' varient='success'>Directions to the market</Button>
+                </Card.Body>
             </Card>
         </>
     )
 }
+
+/*
+    Tellico Plains, Ebaneezer,
+*/
